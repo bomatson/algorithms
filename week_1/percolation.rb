7100395bd7815db0e13d0f59398b4cb54e411cee
bomatson <bobby@carbonfive.com>
2015-02-08 21:21:53 -0800

    refactor to use two dimensional arrays, as well as virtual sites

--- week_1/percolation.rb
+++ week_1/percolation.rb
@@ -7,52 +7,64 @@ class Percolation
   def initialize(n)
     @row_length = n
     @size = n * n
-    @strategy = WeightedQuickUnion.new(@size)
-    @sites = Array.new @size, [1,1,false]
-    build_grid
-  end
+    @strategy = WeightedQuickUnion.new(@size + 2)
+    @sites = Array.new(@row_length) { Array.new(@row_length, 0) }
+    @virtual_top = @size
+    @virtual_bottom = @size + 1
 
-  def is_open?(row, col)
-    site = @sites.detect { |site| site[0] == row && site[1] == col}
-
-    if site
-      site[2]
+    @sites.each_with_index do |ary, index|
+      @strategy.union(@virtual_top, index)
+      @strategy.union(@virtual_bottom, @size - 1 - index)
     end
   end
 
-  def open(row, col)
-    unless is_open?(row, col)
-      site = @sites.detect { |site| site[0] == row && site[1] == col}
-      site[2] = true
+  def is_open?(row, col)
+    @sites[row - 1][col - 1] == 1
+  end
 
-      neighbors = [[row-1, col], [row+1, col], [row, col-1], [row, col+1]]
+  def open(row, col)
+    row_index = row - 1
+    col_index = col - 1
+
+    unless is_open?(row, col)
+      site_id = (row_index * @row_length) + col_index
+
+      neighbors = [[row_index-1, col_index],
+                   [row_index+1, col_index],
+                   [row_index, col_index-1],
+                   [row_index, col_index+1]]
 
       neighbors.each do |neighbor|
+        next if neighbor[0] < 0 || neighbor[0] > @row_length || neighbor[1] < 0 || neighbor[1] > @row_length
+
         if is_open?(neighbor[0], neighbor[1])
-          @strategy.union(@sites.index(neighbor.push(true)), @sites.index(site))
+          # why did I need to normalize here?
+          neighbor_site_id = ((neighbor[0] - 1) * @row_length) + (neighbor[1] - 1)
+
+          if !@strategy.connected?(site_id, neighbor_site_id)
+            @strategy.union(site_id, neighbor_site_id)
+          end
+
+          if is_full?(neighbor[0] + 1, neighbor[1] + 1)
+            @strategy.union(@virtual_top, site_id)
+          end
         end
       end
+
+      @sites[row_index][col_index] = 1
     end
   end
 
   def is_full?(row, col)
-    return false unless is_open?(row, col)
+    row_index = row - 1
+    col_index = col - 1
+    site_id = (row_index * @row_length) + col_index
 
-    top_row_indexes = (0..@row_length - 1).to_a
-
-    top_row_indexes.each do |index|
-      return @strategy.connected?(index, @sites.index([row, col, true]))
-    end
-
-    false
+    is_open?(row, col) && @strategy.connected?(site_id, @virtual_top)
   end
 
   def percolates?
-    bottom_row = @sites.last(@row_length)
-
-    bottom_row.each do |site|
-      return is_full?(site[0], site[1])
-    end
+    @strategy.connected?(@virtual_top, @virtual_bottom)
   end
 
   def monte_carlo
@@ -60,27 +72,6 @@ class Percolation
       open(site[0], site[1])
 
       break if percolates?
-    end
-
-    @sites.select{ |site| site[2] == true }.count.to_f / @size
-  end
-
-  private
-
-  def build_grid
-    col_interval = 1
-    row_interval = 1
-
-    @sites.map! do |site|
-      position = [row_interval, col_interval, false]
-      col_interval += 1
-
-      if col_interval > @row_length
-        col_interval = 1
-        row_interval += 1
-      end
-
-      position
     end
   end
 end
--- week_1/percolation_spec.rb
+++ week_1/percolation_spec.rb
@@ -9,20 +9,8 @@ describe Percolation do
       expect(subject.size).to eq 100
     end
 
-    it 'sets the sites to be an array size N' do
-      expect(subject.sites.count).to eq 100
-    end
-
-    context 'given a 5 x 5 grid' do
-      subject { Percolation.new 5 }
-
-      it 'sets the first position as 1,1' do
-        expect(subject.sites.first).to eq [1,1,false]
-      end
-
-      it 'sets the last position to 5,5' do
-        expect(subject.sites.last).to eq [5,5,false]
-      end
+    it 'sets the sites to be a two dimensional array' do
+      expect(subject.sites.sample).to be_kind_of Array
     end
   end
 
@@ -37,10 +25,10 @@ describe Percolation do
 
     context 'given an open site' do
       before do
-        subject.sites.last[2] = true
+        subject.sites[4][4] = 1
       end
 
-      it 'returns false' do
+      it 'returns true' do
         expect(subject.is_open?(5,5)).to eq true
       end
     end
@@ -69,7 +57,7 @@ describe Percolation do
     end
 
     context 'given a connection to the top row' do
-      it 'returns false' do
+      it 'returns true' do
         subject.open(1,1)
         subject.open(2,1)
         expect(subject.is_full?(2,1)).to eq true
@@ -98,14 +86,6 @@ describe Percolation do
         subject.open(5,1)
         expect(subject.percolates?).to eq true
       end
-    end
-  end
-
-  xdescribe '#monte_carlo?' do
-    subject { Percolation.new 40 }
-
-    it 'returns the fraction of sites open at percolation' do
-      expect(subject.monte_carlo).to eq 0.6475
     end
   end
 end
