d191c8f9bc16e86b448cd2de7f510c6b443c99c0
bomatson <bobby@carbonfive.com>
2015-02-13 16:59:33 -0800

    the ol college try

--- week_2/randomized_queue.rb
+++ week_2/randomized_queue.rb
@@ -1,95 +1,36 @@
-require 'byebug'
-require_relative 'iterator'
-
 class RandomizedQueue
-  attr_reader :head, :tail, :node_count
+  attr_reader :items, :tail
 
-  Node = Struct.new(:item, :prev, :next)
-
-  def initialize(n = 0)
-    @head = nil
-    @tail = nil
-    @node_count = n
-  end
-
-  def enqueue(item)
-    if @head
-      @head.prev = Node.new(item, nil, @head)
-      @head = @head.prev
-    else
-      @head = @tail = Node.new(item)
-    end
-
-    @node_count += 1
-  end
-
-  def is_empty?
-    @head == nil
+  def initialize
+    @items = []
+    @tail = 0
   end
 
   def size
-    @node_count
+    @tail
   end
 
-  def sample
-    shuffled_array[@node_count - 1].item
+  def enqueue(item)
+    @items[@tail] = item
+    @tail += 1
   end
 
   def dequeue
-    shuffled_array
-    rethread_with(shuffled_array)
+    if @tail > 0
+      random_index = rand(@tail)
 
-    item = @tail.item
-    @tail = @tail.prev
+      item_to_dequeue = @items[random_index]
+      @items[random_index] = @items[@tail]
+      @items[@tail] = nil
+      @tail -= 1
 
-    @node_count -= 1
-    item
-  end
-
-  def iterator
-    shuffled_array
-    rethread_with(shuffled_array)
-
-    @iterator ||= Iterator.new(@head)
-  end
-
-  private
-
-  def rethread_with(array)
-    @head = @tail = nil
-    @node_count = 0
-
-    array.each do |node|
-      enqueue(node.item)
+      item_to_dequeue
     end
   end
 
-  def shuffled_array
-    array = []
-    current = @head
+  def sample
+    random_index = rand(@tail)
 
-    @node_count.times do |i|
-      array[i] = current
-      current = current.next
-    end
-
-    shuffle!(array)
-
-    array
-  end
-
-  def shuffle!(array)
-    n = array.size
-
-    n.times do |i|
-      random = rand(i + 1)
-      exchange(array, i, random)
-    end
-  end
-
-  def exchange(array, i, j)
-    swap = array[i]
-    array[i] = array[j]
-    array[j] = swap
+    @items[random_index]
   end
 end
